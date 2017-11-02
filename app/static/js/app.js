555f314943d88c5583d4006ec4ceeca94555fe95
Lorenzo Baronio <lorenzo.baronio@gmail.com>
2017-11-02 12:18:17 +0100

    Primo commit: riscrittura da versione django

---
+++ app/static/js/app.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    // sidebar animation handler
+    $('#open-close').click(openCloseSidebar);
+    $('#sidebar .w3-button').click(hideSidebar);
+
+    // facebook icon animation handler
+    $('.facebook').hover(facebookHover, facebookOut);
+
+    // play carousel
+    myIndex = 0;
+    carousel();
+
+});
+
+// sidebar animation
+function openCloseSidebar() {
+    if ($('#sidebar').is(':visible')) {
+        $(this).html('&#9776;');
+        $('#sidebar').css('display', 'none');
+    } else {
+        $(this).html('&times;');
+        $('#sidebar').css('display', 'block');
+    }
+}
+function hideSidebar() {
+        $('#open-close').html('&#9776;');
+        $('#sidebar').css('display', 'none');
+}
+
+// facebook icon animation
+function facebookHover() {
+    $(this).attr('src', "/static/img/Facebook.png");
+}
+function facebookOut() {
+    $(this).attr('src', "/static/img/Facebook-Over.png");
+}
+
+// carousel animation
+function carousel() {
+    var i;
+    var x = $(".slides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 2500);
+}
