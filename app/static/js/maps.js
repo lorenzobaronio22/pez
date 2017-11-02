c1f5fe567ed768b08d7c8f775de06997cbfbf0a9
Lorenzo Baronio <lorenzo.baronio@gmail.com>
2017-11-02 19:11:40 +0100

    Aggiunta form per email

--- app/static/js/maps.js
+++ app/static/js/maps.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     showLocation("#mappa-reggio", 44.7672772, 10.4316505);
-    showLocation("#mappa-parma", 44.8454213, 10.4441278);
+    showLocation("#mappa-parma", 44.8452426, 10.446284099999957); // Via Santi Faustino e Giovita, 14/16, 43058 Sorbolo PR
 });
 
 function createStaticMarker( markerColor, markerLabel, lat, lng ){
